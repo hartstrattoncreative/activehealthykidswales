b8dd937d997cab6f9af2ac194448fc3acc8d9a02
Owain Stratton <owain@hsc.studio>
2022-10-22 15:47:57 +0100

    feat: add locale support

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,11 @@ const withTM = require('next-transpile-modules')([]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
+  i18n: {
+    locales: ['en', 'cy'],
+    defaultLocale: 'en',
+  },
 });
