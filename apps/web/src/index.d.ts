d48a19c456598a2cc835f9801632429237d0730d
Owain Stratton <owain@hsc.studio>
2022-10-22 21:56:10 +0100

    feat: add next-sanity and setup

---
+++ apps/web/src/index.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_SANITY_DATASET: string;
+      NEXT_PUBLIC_SANITY_PROJECT_ID: string;
+      SANITY_API_TOKEN: string;
+    }
+  }
+}
