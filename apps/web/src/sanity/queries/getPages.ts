7b7dc13a27b1ad223f263694ccfa990395078b95
anghar <angelica@hsc.studio>
2022-10-27 21:53:55 +0100

    feat: adds settings types

--- apps/web/src/sanity/queries/getPages.ts
+++ apps/web/src/sanity/queries/getPages.ts
@@ -1,6 +1,6 @@
 import { groq } from 'next-sanity';
 import { getClient } from 'sanity/server';
-import { SlugPages } from 'sanity/types';
+import { Page } from 'sanity/types/documents';
 
 const query = groq`
 *[_type == "page" && defined(slug) && !(_id in path('drafts.**')) && !(_id in [
@@ -11,5 +11,5 @@ const query = groq`
 `;
 
 export default async function getPages(preview: boolean = false) {
-  return getClient(preview).fetch<SlugPages>(query);
+  return getClient(preview).fetch<Page[]>(query);
 }
--- apps/web/src/sanity/types/documents.ts
+++ apps/web/src/sanity/types/documents.ts
@@ -2,10 +2,19 @@ import { SanityBase } from './base';
 
 export type Page = {
   slug: string;
-  modules: any[]; // TODO: update any
+  modules: any[]; // TODO: type modules
   metaTitle: string;
   metaDescription?: string;
   disallowRobots?: boolean;
+};
+
+export type SiteSettings = SanityBase & {
+  title: string;
+  lang: string[];
+  logo: any; // TODO: type asset
+  cookieConsent: any; // TODO: type notification
+  metaTitle: string;
+  metaDescription: string;
 };
 
 export type Menu = SanityBase & {
